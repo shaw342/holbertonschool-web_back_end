93a08a24a6bfc76a4d59532cf97aaf575af49659
VotreNom <shawanbarua70@gmail.com>
2023-08-28 15:43:08 +0200

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!usr/bin/env python3
+"""Basic annotations - concat funtion"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Write a type-annotated function concat that takes a string str1 and
+     a string str2 as arguments and returns a concatenated string """
+    return "{}{}".format(str1, str2)/
