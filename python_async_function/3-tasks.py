274820ee52a879fe4353d8e55be284a5771fb867
VotreNom <shawanbarua70@gmail.com>
2023-08-29 17:44:48 +0200

    del .py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,7 +2,7 @@
 """asyncio task"""
 import asyncio
 
-wait_random = __import__("0-basic_async_syntax.py").wait_random
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
