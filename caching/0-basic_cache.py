33caf66c51f9470afd22f31ebe4d3ecadeb9849c
shaw342 <shawanbarua70@gmail.com>
2024-06-14 13:02:31 +0200

    pycode style

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python3
+""" BasicCache module
+"""
 from basic_caching import BaseCaching
 
-class BasicCache(BaseCaching):
 
-    def put(self, key,item):
-        self.cache_data[key] = item
+class BasicCache(BaseCaching):
+    """ Caching system BasicCache that inherits from BaseCaching.
+    """
+    def put(self, key, item):
+        """ Assign to the self.cache_data the item value for the key.
+            If key or item is None, this method should not do anything.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        return self.cache_data.get(key)
-                
+        """ Return the value in self.cache_data linked to key.
+            If key is None or if the key doesn’t exist, return None.
+        """
+        return self.cache_data.get(key, None)
