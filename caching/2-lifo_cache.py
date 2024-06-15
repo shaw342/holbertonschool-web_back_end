75c42af2ff2a30f19681ea15d396ab0b2bb96886
shaw342 <shawanbarua70@gmail.com>
2024-06-15 18:55:35 +0200

    refactor code

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,30 +1,28 @@
-"""
-Lifo cache module
-"""
+'''
+basic cache
+'''
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """
-    Lifo Class
-    """
-
+    """ Basic cache class """
     def __init__(self):
-        """ initilize"""
         super().__init__()
 
     def put(self, key, item):
-        """ method put value with key"""
-        if key is None and item is None:
+        """ Put a key/value pair """
+        if key is None or item is None:
             return
 
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             last_item_key = list(self.cache_data.keys())[-1]
             print(f"DISCARD: {last_item_key}")
-            self.cache_data.pop(last_item_key)
+            del self.cache_data[last_item_key]
 
         self.cache_data[key] = item
 
     def get(self, key):
-        """ method get for get value with key"""
-        return self.cache_data.get(key,None)
+        """ Get a key/value pair """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
