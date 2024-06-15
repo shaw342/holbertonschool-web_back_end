9dec91dd51b5eccec3e3484d612a3e66d10abb99
shaw342 <shawanbarua70@gmail.com>
2024-06-15 18:22:28 +0200

    lifo algo

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,26 @@
+"""Lifo cache module"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+"""Lifo Class """
+    def __init__(self):
+        """ initilize"""
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """ method put value with key"""
+        if key is None and item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_item_key = list(self.cache_data.keys())[-1]
+            self.cache_data.pop(last_item_key)
+            print(f"DISCARD: {last_item_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ method get for get value with key"""
+        return self.cache_data.get(key)
