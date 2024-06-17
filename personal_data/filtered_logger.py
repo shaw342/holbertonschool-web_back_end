2cf6057d289c9c9ca45a0f2833a93f971a4ef958
shaw342 <shawanbarua70@gmail.com>
2024-06-17 22:09:15 +0200

    forget add readme

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,19 +3,7 @@ import re
 
 def filter_datum(fields: list, redaction: str,
                  message: str, separator: str) -> str:
-    """
-    Retourne un message de journal obfusqué.
-
-    Args:
-        fields (list): Liste des champs à obfusquer.
-        redaction (str): Valeur d'obfuscation.
-        message (str): Ligne de journal.
-        separator (str): Caractère séparant
-        les champs dans la ligne de journal.
-
-    Returns:
-        str: Message de journal obfusqué.
-    """
+    """ function fileter_datume """
     for field in fields:
         message = re.sub(f"{field}=[^;{separator}]*",
                          f"{field}={redaction}", message)
