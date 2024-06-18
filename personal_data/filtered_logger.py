93bdcae6e875c9be9ade6b42c230851cf0a4c958
shaw342 <shawanbarua70@gmail.com>
2024-06-18 10:25:54 +0200

    add type

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """ function fileter_datume """
     for field in fields:
