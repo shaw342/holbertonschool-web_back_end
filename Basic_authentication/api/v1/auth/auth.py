2b27938ee0985385f87bbece4de213265047173f
shaw342 <shawanbarua70@gmail.com>
2024-06-22 11:11:58 +0200

    auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python3
-from typing import List, TypeVar
+"""
+Authentication module for the API
+"""
 from flask import request
+from typing import List, TypeVar
 
 
-class Auth:
+User = TypeVar('User')
+
+
+class Auth():
+    """ Class to manage the API authentication.
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        """ Determines if authentication is required for the given path.
+        """
+        if path is None:
+            return True
+
+        if not excluded_paths:
+            return True
+
+        normal_path = path if path.endswith('/') else path + '/'
+
+        normal_excluded_paths = [p if p.endswith('/') else p + '/'
+                                 for p in excluded_paths]
+
+        if normal_path in normal_excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        return request
+        """ Retrieves the Authorization header from the request.
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        return request
+    def current_user(self, request=None) -> User:
+        """ Retrieves the current user from the request.
+        """
+        return None
