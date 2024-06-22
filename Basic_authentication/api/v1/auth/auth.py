5a788b12984d59665e62cbbda4e74c0d978cecbe
shaw342 <shawanbarua70@gmail.com>
2024-06-22 10:11:38 +0200

    pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -2,13 +2,13 @@
 from typing import List, TypeVar
 from flask import request
 
+
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         return request
-        
-    def current_user(self, request=None) -> TypeVar('User'): 
+
+    def current_user(self, request=None) -> TypeVar('User'):
         return request
-    
