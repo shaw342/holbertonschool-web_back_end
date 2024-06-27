ebb4d2313fec7df5fa064bedc475c7f2bea49244
shaw342 <shawanbarua70@gmail.com>
2024-06-27 11:58:18 +0200

    user

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """user Table"""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
