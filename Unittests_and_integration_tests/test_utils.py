bc69661c35667785f1cec5dd5d554bc2defbbaef
shaw342 <shawanbarua70@gmail.com>
2024-07-04 18:16:15 +0200

    change path for first test

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,75 +1,24 @@
 #!/usr/bin/env python3
-"""File for testing utils.py"""
-
-import unittest
-from unittest.mock import patch, Mock
+"""
+test file
+"""
 from parameterized import parameterized
-from utils import access_nested_map, get_json, memoize
+import unittest
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test Access Nested Map Class"""
+    """test class"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": 1},("a", ), 1),
+        ({"a": {"b": 2}}, ("a", ), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, map, path, expected_output):
-        """Test Access Nested Map Method"""
-        real_output = access_nested_map(map, path)
-        self.assertEqual(real_output, expected_output)
-
-    @parameterized.expand([
-        ({}, ("a",), 'a'),
-        ({"a": 1}, ("a", "b"), 'b')
-    ])
-    def test_access_nested_map_exception(self, map, path, wrong_output):
-        """Test Access Nested Map Exception Method"""
-        with self.assertRaises(KeyError) as e:
-            access_nested_map(map, path)
-            self.assertEqual(wrong_output, e.exception)
-
-
-class TestGetJson(unittest.TestCase):
-    """Test Get JSON Class"""
-
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    def test_get_json(self, test_url, test_payload):
-        """Test Get JSON Method"""
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        with patch('requests.get', return_value=mock_response):
-            real_response = get_json(test_url)
-            self.assertEqual(real_response, test_payload)
-            mock_response.json.assert_called_once()
-
-
-class TestMemoize(unittest.TestCase):
-    """Test Memoize Class"""
-
-    def test_memoize(self):
-        """Test Memoize Method"""
-
-        class TestClass:
-            """Test Class"""
-
-            def a_method(self):
-                """A Method"""
-                return 42
-
-            @memoize
-            def a_property(self):
-                """A Property Method"""
-                return self.a_method()
-
-        with patch.object(TestClass, 'a_method', return_value=42) as patched:
-            test_class = TestClass()
-            real_return = test_class.a_property
-            real_return = test_class.a_property
-
-            self.assertEqual(real_return, 42)
-            patched.assert_called_once()
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Implement the
+        TestAccessNestedMap.test_access_nested_map method
+        """
+        result = access_nested_map(nested_map=nested_map, path=path)
+        self.assertEqual(result, expected)
