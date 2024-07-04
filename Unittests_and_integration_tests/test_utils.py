0a65d1890c3298767658292295740bcd0d2992bc
shaw342 <shawanbarua70@gmail.com>
2024-07-04 17:36:06 +0200

    change result for path a

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -11,8 +11,8 @@ class TestAccessNestedMap(unittest.TestCase):
     """test class"""
     
     @parameterized.expand([
-        ("a",1)
-        ({"a":{"b": 2}},"a",2)
+        ("a",1),
+        ({"a":{"b": 2}},("a",),{"b":2}),
         ({"a": {"b": 2}},("a","b"),2)
     ])
     def test_access_nested_map(self,nested_map, path, expected):
