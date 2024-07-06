3bf6eb2f9b78d0d22e44be201965ffce662d5951
shaw342 <shawanbarua70@gmail.com>
2024-07-06 15:49:00 +0200

    handle the requirement

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template
+#!/usr/bin/env python3
+"""python file handle the all route"""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
+    """routet return the templates of homepage"""
     return render_template("./index.html")
-
 
 
 if __name__ == "__main__":
