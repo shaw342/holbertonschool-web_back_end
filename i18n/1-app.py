486f0cd5084a0893d5d3694ab12e6f48e4d06858
shaw342 <shawanbarua70@gmail.com>
2024-07-07 12:13:09 +0200

    changer file path

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
-""" flask module handle route
+""" Basic Flask app Module
 """
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
-babel = Babel(app=app)
+babel = Babel(app)
 
 
 class Config:
-    """config class"""
+    """ Configuration class.
+    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
-    TIMEZON = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config(Config)
+app.config.from_object(Config)
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def home():
-    """routet return the templates of homepage"""
-    return render_template("./1-index.html")
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """Endpoint returning Hello world.
+    """
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
