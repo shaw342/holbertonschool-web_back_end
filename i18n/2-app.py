f0858b58dff01effec081b32aed0a7ee415a0b5f
shaw342 <shawanbarua70@gmail.com>
2024-07-07 13:10:44 +0200

    refactor the code é

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -5,8 +5,10 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 class Config:
     """ Configuration class.
@@ -15,7 +17,9 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def welcome() -> str:
@@ -23,13 +27,12 @@ def welcome() -> str:
     """
     return render_template("2-index.html")
 
+
 @babel.localeselector
 def get_locale() -> str:
-    """Determine the best language code for the user.
-    """
-    accepted_languages = request.accept_languages
-    best_match = accepted_languages.best_match(Config.LANGUAGES)
-    return best_match
+    """Select the best match language."""
+    return request.accept_languages.best_match(["en", "fr"])
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
