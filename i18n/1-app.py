d5875492abce9a92213143fba35db8f5350dc8c0
shaw342 <shawanbarua70@gmail.com>
2024-07-07 12:03:39 +0200

    1-app

---
+++ i18n/1-app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app=app)
+
+
+class config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    TIMEZON = "UTC"
+
+
+app.config(config)
+
+
+@app.route("/")
+def home():
+    """routet return the templates of homepage"""
+    return render_template("./index.html")
+
+
+if __name__ == "__main__":
+    app.run()
