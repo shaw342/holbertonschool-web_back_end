e756180ff8215d22ec84825fbeb7e7bcf4ac5fee
shaw342 <shawanbarua70@gmail.com>
2024-07-07 14:12:25 +0200

    install rigth version flask

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
-""" Basic Flask app Module
-"""
+"""Basic Flask app, index page"""
 
-from flask import Flask, render_template, request
-from flask_babel import Babel
+# Import necessary modules
+from flask import Flask, request, render_template
+from flask_babel import Babel, _
 
-
+# Create your Flask app and configure Babel
 app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config:
-    """ Configuration class.
+    """Config class for app configuration
     """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
-
-@app.route('/', methods=['GET'], strict_slashes=False)
-def welcome() -> str:
-    """Endpoint returning Hello world.
-    """
-    return render_template("./2-index.html")
+babel = Babel(app)
 
 
+# Define the get_locale function
 @babel.localeselector
-def get_local() -> str:
-    """get_local"""
-    return request.accept_languages.best_match(app.Config["LANGUAGES"])
+def get_locale():
+    """Get the best-matched language based on the user's preferences
+    """
+    # Determine the best-matched language based on the user's preferences
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    "home page"
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
