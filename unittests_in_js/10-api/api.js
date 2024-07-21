29ef459d28d8f87a0c8225faabc9a460b7118eb8
shaw342 <shawanbarua70@gmail.com>
2024-07-21 15:13:27 +0200

    task 10

---
+++ unittests_in_js/10-api/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+    const { id } = req.params;
+    res.send(`Payment methods for cart ${id}`);
+  });  
+
+app.get("/available_payments",(req,res) =>{
+    res.send(req.params)
+})
+
+const port = 7865;
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
