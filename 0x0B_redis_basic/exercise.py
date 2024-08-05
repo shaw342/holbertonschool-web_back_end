222b0a051db306561bf7002ef94bb2a21cb1be30
shaw342 <shawanbarua70@gmail.com>
2024-08-05 14:23:28 +0200

    first for method  store data

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import redis
+import uuid
+from typing import Union
+
+class Cache():
+    def __init__(self) -> None:
+        self._redis = redis.Redis(host="localhost",port=6379)
+        
+    def store(self,data: Union[str,bytes,float,int]) -> str:
+        key =  str(uuid.uuid4())
+        self._redis.set(key,data)
+        return key
+    
+
