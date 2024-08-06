f8dc2f0c24d19a1543b30a3732ed1e40db9d482e
shaw342 <shawanbarua70@gmail.com>
2024-08-06 12:03:27 +0200

    us flushdb

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,13 +3,13 @@ import redis
 import uuid
 from typing import Union
 
+
 class Cache():
     def __init__(self) -> None:
-        self._redis = redis.Redis(host="localhost",port=6379)
+        self._redis = redis.Redis()
+        self._redis.flushdb()
         
-    def store(self,data: Union[str,bytes,float,int]) -> str:
+    def store(self,data: Union[str, bytes, float, int]) -> str:
         key =  str(uuid.uuid4())
-        self._redis.set(key,data)
+        self._redis.set(key, data)
         return key
-    
-
