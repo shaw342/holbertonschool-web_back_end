0af8da44e596213c5b5a18cc85a35cd4d8bf5aaf
shaw342 <shawanbarua70@gmail.com>
2024-08-08 11:15:13 +0200

    add comment ans exit strict mode

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,8 @@
+-- Create the 'users' table if it does not already exist
+-- 'id' column: auto-incrementing primary key
+-- 'email' column: unique and non-null string
+-- 'name' column: string
+
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
