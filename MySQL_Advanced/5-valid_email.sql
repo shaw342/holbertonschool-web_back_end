f0bef1834ba96873f903ae9a7e8ab8ca2503de4c
shaw342 <shawanbarua70@gmail.com>
2024-08-11 15:31:02 +0200

    set new valid email

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER `valid_email` BEFORE UPDATE
 ON `users` FOR EACH ROW 
 BEGIN
     IF NEW.email <> OLD.email THEN
-    valid_email = 0
+    SET NEW.valid_email = 0
     END IF;
 
 END $$
