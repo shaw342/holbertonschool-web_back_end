89fcb9db529ea1eff5e7b70473979c3fe334dbc9
shaw342 <shawanbarua70@gmail.com>
2024-08-11 15:29:41 +0200

    verify valid email

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
+
+DELIMIER $$
+CREATE TRIGGER `valid_email` BEFORE UPDATE
+ON `users` FOR EACH ROW 
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    valid_email = 0
+    END IF;
+
+END $$
+DELIMITER;
